fabdd48d170471f3b6e454a5c463e3821e416a7a
Dylan Whichard <dylan@whichard.com>
2024-08-23 20:28:55 -0700

    Add client and port name for alsa

--- platform_midi_alsa.h
+++ platform_midi_alsa.h
@@ -171,16 +171,20 @@ void platform_midi_list_devices_alsa(struct platform_midi_driver *driver)
     // Loop over all the clients
     while (0 == snd_seq_query_next_client(alsa_driver->seq_handle, client_info))
     {
-        int clientId = snd_seq_client_info_get_client(client_info);
-        printf("Client: %d\n", clientId);
+        int client_id = snd_seq_client_info_get_client(client_info);
+        const char* client_name = snd_seq_client_info_get_name(client_info);
+        printf("%s\n", client_name);
 
         // Use -1 to specify the first port on this client
         snd_seq_port_info_set_port(port_info, -1);
-        snd_seq_port_info_set_client(port_info, clientId);
+        snd_seq_port_info_set_client(port_info, client_id);
         while (0 == snd_seq_query_next_port(alsa_driver->seq_handle, port_info))
         {
-            printf("Port: %d\n", snd_seq_port_info_get_port(port_info));
+            int port_id = snd_seq_port_info_get_port(port_info);
+            const char* port_name = snd_seq_port_info_get_name(port_info);
+            printf(" - %d:%d '%s'\n", client_id, port_id, port_name);
         }
+        printf("\n");
     }
 }
 #endif
